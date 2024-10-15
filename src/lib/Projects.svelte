1da7cae2e91c7c82461492401f0ba36b4bdea87b
James Reed <james@twiddlingbits.net>
2024-10-15 11:41:11 -0600

    Rework projects section

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-    import '@splidejs/svelte-splide/css';
-
-    import { Splide, SplideSlide, SplideTrack } from '@splidejs/svelte-splide'
+    import RightArrow from '~icons/material-symbols/arrow-right-alt-rounded'
 
     import projectData from './data/projects.json'
 
@@ -14,49 +12,36 @@
     }
 </script>
 
-<div id="#projects" class="scroll-mt-32 flex flex-col gap-20">
+<div id="#projects" class="scroll-mt-32 w-full flex flex-col items-center gap-20">
     <div class="divider text-[6em] italic font-light">
         Projects
     </div>
-    <Splide hasTrack={false} options={{ pagination: false }}>
-        <div>
-            <div class="relative top-12 splide__arrows" />
-            <SplideTrack>
-            {#each projectData as project}
-                <SplideSlide>
-                    <div class="flex flex-col gap-20 items-center">
-                        <div class="flex flex-col gap-6">
-                            <div class="flex flex-col gap-4">
-                                <div class="flex items-center gap-4">
-                                    <a href={project.url} class="link text-5xl font-bold">{project.name}</a>
-                                    <span class={`mt-3 badge ${statusColors[project.statusColor]}`}>{project.status}</span>
-                                </div>
-                                <div class="text-xl">
-                                    {project.description}
-                                </div>
-                            </div>
-                            <div class="prose">
-                                <ul>
-                                    {#each project.info as infoText}
-                                        <li>
-                                            {@html infoText}
-                                        </li>
-                                    {/each}
-                                </ul>
-                            </div>
-                            <div class="flex gap-2">
-                                {#each project.tech as techName}
-                                    <span class="badge">{techName}</span>
-                                {/each}
-                            </div>
-                        </div>
-                        <div class="flex justify-center border">
-                            <img src={project.imageUrl} class="w-3/4 object-contain" />
-                        </div>
-                    </div>
-                </SplideSlide>
-            {/each}
-            </SplideTrack>
+    <div class="flex flex-col w-full max-w-screen-lg gap-x-8 gap-y-12">
+        {#each projectData as project, idx}
+        <div class={`flex gap-8 ${idx % 2 ? 'flex-row-reverse' : 'flex-row'} rounded-2xl p-8`}>
+            <div class="flex flex-col gap-6 justify-center">
+                <div class="flex gap-2 items-center">
+                    <a href={project.url} class="text-5xl font-bold">{project.name}</a>
+                    <RightArrow style="font-size:1.75em;margin-top:14"/>
+                </div>
+                <div class="text-xl">
+                    {project.description}
+                </div>
+                <div class="flex gap-2 flex-wrap">
+                    {#each project.tech as techName}
+                        <span class="badge">{techName}</span>
+                    {/each}
+                </div>
+            </div>
+            <span class="py-20 divider divider-horizontal"></span>
+            <a href={project.url} class="w-full relative">
+                <div class={`${idx % 2 ? 'left-4' : 'right-4'} flex gap-2 text-lg p-3 badge absolute top-4`}>
+                    <span class={`${statusColors[project.statusColor]} badge badge-xs`}></span>
+                    {project.status}
+                </div>
+                <img src={project.imageUrl} class={`${idx % 2 ? 'rounded-l-2xl' : 'rounded-r-2xl'} border-2 border-black object-cover`} />
+            </a>
         </div>
-    </Splide>
+        {/each}
+    </div>
 </div>
