57eb6cb4e9f4c6ecd4da99f9acf94c58c7478709
James Reed <james@twiddlingbits.net>
2024-10-16 15:49:14 -0600

    Fix navbar text size

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -6,22 +6,22 @@
     import RightArrow from '~icons/material-symbols/arrow-right-alt-rounded'
 </script>
 
-<nav class="md:text-xl z-50 navbar w-screen flex justify-center gap-4 bg-white fixed top-0">
+<nav class="z-50 navbar w-screen flex justify-center gap-4 bg-white fixed top-0">
     <a
         href="#about"
-        class="btn btn-ghost"
+        class="sm:text-lg md:text-xl lg:text-2xl btn btn-ghost"
         class:btn-active={$page.url.hash === "#about"}
     >
         About
     </a>
     <a
         href="#projects"
-        class="btn btn-ghost"
+        class="sm:text-lg md:text-xl lg:text-2xl btn btn-ghost"
         class:btn-active={$page.url.hash === "#projects"}
     >
         Projects
     </a>
-    <a class="btn btn-ghost" href={PUBLIC_BLOG_URL}>
+    <a class="sm:text-lg md:text-xl lg:text-2xl btn btn-ghost" href={PUBLIC_BLOG_URL}>
         Blog
         <RightArrow style="margin-top:4" />
     </a>
