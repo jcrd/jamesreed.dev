db84845dbdf6021068551bb28f558d9f976d0012
James Reed <james@twiddlingbits.net>
2024-10-20 16:25:39 -0600

    Add github link

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-    import { PUBLIC_BLOG_URL } from '$env/static/public'
+    import { PUBLIC_BLOG_URL, PUBLIC_GITHUB_PROFILE_URL } from '$env/static/public'
 
     import RightArrow from '~icons/material-symbols/arrow-right-alt-rounded'
 
     import projectData from './data/projects.json'
+    import iconData from './data/icons.json'
 
     projectData.sort((a, b) => a.order - b.order)
 
@@ -48,8 +49,17 @@
             {/if}
         {/each}
     </div>
-    <a href={PUBLIC_BLOG_URL} class="flex gap-2 items-center text-lg md:text-2xl font-semibold mt-12">
-        Find more projects on my blog
-        <RightArrow style="" />
-    </a>
+    <div class="flex flex-col sm:flex-row gap-2 sm:h-12 mt-12">
+        <a href={PUBLIC_GITHUB_PROFILE_URL} class="flex gap-2 items-center text-2xl font-semibold">
+            <RightArrow style="transform:rotate(180deg)" />
+            <img src={iconData.github} alt="GitHub icon" />
+            Find more
+        </a>
+        <span class="divider sm:divider-horizontal text-gray-700"></span>
+        <a href={PUBLIC_BLOG_URL} class="flex gap-2 items-center text-2xl font-semibold">
+            Read more
+            <img src={iconData.blog} alt="Blog icon" />
+            <RightArrow style="" />
+        </a>
+    </div>
 </div>
